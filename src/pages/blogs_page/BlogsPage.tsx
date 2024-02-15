933b3f9542823375f308612e5ce2cc32055d2743
hackbyte07 <aayushranahardi@gmail.com>
2024-02-15 11:14:17 +0530

    working on blogs screen;

---
+++ src/pages/blogs_page/BlogsPage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Navbar from "../../components/Navbar";
+import {
+  BlogsInitialStateType,
+  fetchBlogs,
+} from "../../redux/blogsSlice/blogsSlice";
+
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store";
+import BlogsCard from "../../components/BlogsCard";
+
+const BlogsPage = () => {
+  const blogsSelector = useSelector<RootState>(
+    (state) => state.blogsSlice
+  ) as BlogsInitialStateType;
+
+  const appDispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    appDispatch(fetchBlogs());
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 place-items-center">
+        {blogsSelector.data.success === true &&
+          blogsSelector.data.data.rows
+            .filter((it) => it.thumbnail !== "")
+            .map((it) => (
+              <li key={it.id} className="my-2.5">
+                <BlogsCard
+                  image={it.thumbnail}
+                  title={it.title}
+                  body={it.description.slice(3, -5)}
+                  url={it.link}
+                />
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BlogsPage;
